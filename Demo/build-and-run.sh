ba5363e1596668fe3f5f529b985852dd1c64eb06
John Susek <john@johnsolo.net>
2025-12-06 07:16:02 -0600

    quiet

--- Demo/build-and-run.sh
+++ Demo/build-and-run.sh
@@ -15,7 +15,7 @@ bash -lc 'cfg='debug'; src=.build/$cfg; dest=GodotProject/bin; mkdir -p "$dest";
 bash -c 'rm -f ../*.{d,dia,swiftdeps,swiftmodule}'
 
 # Codesign all dylibs
-codesign --force --deep --sign - GodotProject/bin/*.dylib
+codesign --force --deep --sign - GodotProject/bin/*.dylib 2>/dev/null
 
 # Start Godot - unless "norun" argument is given
 if [ "$1" == "norun" ]; then
