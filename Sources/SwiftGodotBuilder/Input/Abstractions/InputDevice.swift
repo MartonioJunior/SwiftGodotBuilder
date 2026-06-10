528f0de3651bf0b684065ed92789b25a30b3f530
Martônio Júnior <martoniojunior@hotmail.com>
2026-06-10 15:02:07 -0300

    feat(Input): `InputDevice`
    Added `isEmulated` check to evaluate whether a device is emulated or not

--- Sources/SwiftGodotBuilder/Input/Abstractions/InputDevice.swift
+++ Sources/SwiftGodotBuilder/Input/Abstractions/InputDevice.swift
@@ -8,7 +8,11 @@
 /// Abstraction that represents information about an input device.
 public struct InputDevice {
     /// Identifier for the device used.
+    /// 
+    /// Can be negative for special use cases that don't refer to devices physically present on the system.
     public var id: Int
+    /// Is the device emulated?
+    public var isEmulated: Bool { id < 0 }
     /// Creates a new input device.
     /// - Parameter id: Identifier for the device.
     public init(_ id: Int) {
