c500bfd5817886cfbd120233d6726d1d6b26d21d
Martônio Júnior <martoniojunior@hotmail.com>
2026-06-10 17:34:16 -0300

    feat(Input): Input Sources
    Defines an abstraction for a group of possible accepted input states

---
+++ Sources/SwiftGodotBuilder/Input/Base/InputSource.swift
@@ -0,0 +1,32 @@
+//
+//  InputSource.swift
+//  SwiftGodotBuilder
+//
+//  Created by Martônio Júnior on 10/06/2026.
+//
+
+import SwiftGodot
+
+public protocol InputSource {
+    /// Type of input events that can be received.
+    associatedtype Input
+    /// Device associated with the input.
+    var device: InputDevice { get }
+    // MARK: Methods
+    /// Attempts to extracts information from an input event instance.
+    static func unwrapEvent(_ event: InputEvent) -> Input?
+    /// Builds the corresponding Godot `InputEvent` instance.
+    ///
+    /// This materializes the declarative spec into an engine object that
+    /// can be registered with `InputMap`.
+    func wrapInput(_ input: Input) -> InputEvent
+}
+
+// MARK: Default Implementation
+public extension InputSource {
+    func unwrapEvent(_ event: InputEvent) -> Input? {
+        guard event.device == device.id else { return nil }
+
+        return Self.unwrapEvent(event)
+    }
+}
